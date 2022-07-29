943631a6d5980780588a47e7ade8d71cd5ff85d3
khvci <88493840+khvci@users.noreply.github.com>
2022-07-29 09:33:19 +0300

    add mario.c

---
+++ week_1/mario.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void makePyramid(int levels);
+
+void main(int argc, char const *argv[])
+{
+    int levels;
+    do
+    {
+        printf("How many levels do you want to create? ");
+        scanf("%i", &levels);
+
+        if (levels < 1)
+        {
+            printf("Try again. Please provide a positive integer.");
+        }
+    } while (levels < 1);
+    
+    makePyramid(levels);
+}
+
+void makePyramid(int levels)
+{
+    for (int i = 0; i < levels; i++)
+    {
+        for (int j = 0; j < levels - i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
