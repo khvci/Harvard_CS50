33cec34b8031c4b4b306a313a0bef5183f12fabd
khvci <88493840+khvci@users.noreply.github.com>
2022-07-29 09:43:18 +0300

    fix for loop logic

--- week_1/mario.c
+++ week_1/mario.c
@@ -10,10 +10,8 @@ void main(int argc, char const *argv[])
         printf("How many levels do you want to create? ");
         scanf("%i", &levels);
 
-        if (levels < 1)
-        {
-            printf("Try again. Please provide a positive integer.");
-        }
+        if (levels < 1) printf("Try again. Please provide a positive integer.");
+     
     } while (levels < 1);
     
     makePyramid(levels);
@@ -23,15 +21,11 @@ void makePyramid(int levels)
 {
     for (int i = 0; i < levels; i++)
     {
-        for (int j = 0; j < levels - i; j++)
-        {
+        for (int j = 1; j < levels - i; j++) 
             printf(" ");
-        }
 
-        for (int k = 0; k <= i; k++)
-        {
+        for (int k = 0; k <= i; k++) 
             printf("#");
-        }
 
         printf("\n");
     }
